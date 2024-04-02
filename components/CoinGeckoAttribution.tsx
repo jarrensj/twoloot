ef006b19a6e7a932f98fc6eed259ba68f0fb363f
Jarren San Jose <sanjose.jarren@gmail.com>
2024-04-02 13:42:54 -0700

    edit CoinGecko attribution

--- components/CoinGeckoAttribution.tsx
+++ components/CoinGeckoAttribution.tsx
@@ -7,9 +7,6 @@ const CoinGeckoAttribution = () => {
       <p className="text-xs">
         <Link href="https://coingecko.com">Data provided by CoinGecko</Link>
       </p>
-      <Link href="https://coingecko.com">
-       <Image src="/coingecko_logo.png" alt="coingecko logo with black text" width={100} height={100} />
-      </Link>
     </div>
   );
 };
